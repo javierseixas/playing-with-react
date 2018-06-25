215c9ecae29d894353f38e51816b32c36962f978
Javier Seixas <dev@javierseixas.com>
2018-06-24 22:51:16 -0400

    Introduces state

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -4,18 +4,22 @@ export class Home extends React.Component {
 
     constructor(props) {
         super();
-        this.age = props.age;
+        this.state = {
+            age: props.age,
+        };
     }
 
     onMakeMeYounger() {
-        this.age -= 1;
+        this.setState({
+            age: this.state.age - 1
+        });
     }
 
     render() {
         return(
             <div>
                 <h2>Home</h2>
-                <p>My name is {this.props.name} and I'm {this.age}</p>
+                <p>My name is {this.props.name} and I'm {this.state.age}</p>
                 <hr />
                 <button onClick={this.onMakeMeYounger.bind(this)}>Make me younger!</button>
             </div>
