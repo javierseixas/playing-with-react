9f52470fa14dc664320f3d7695efd551e03cd54b
Javier Seixas <dev@javierseixas.com>
2018-06-24 22:20:55 -0400

    Embeds components into Main Component

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
 
+import { Home } from "./components/Home"
+import { Header } from "./components/Header"
+
 class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello!</h1>
+                <Header />
+                <Home />
             </div>
         );
     }
